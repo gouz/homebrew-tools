f1531cdf417a1572b20aed33b39d026e959c100a
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2023-08-29 21:50:54 +0200

    feat: :rocket: add slidesk

---
+++ README.md
@@ -0,0 +1,23 @@
+# homebrew-tools
+
+Homebrew formula for installing gouz's apps & tools.
+
+## How To Use
+
+Add repository:
+
+```
+brew tap gouz/tools
+```
+
+Install [SliDesk](https://github.com/gouz/slidesk):
+
+```
+brew install slidesk
+```
+
+Upgrade the SliDesk CLI to the latest version:
+
+```
+brew upgrade slidesk
+```
---
+++ slidesk.rb
@@ -0,0 +1,14 @@
+class SliDesk < Formula
+    desc "Speaker companion"
+    homepage "https://github.com/gouz/homebrew-tools"
+    version "1.2.2"
+
+    on_macos do
+        url "https://github.com/gouz/slidesk/archive/refs/tags/1.2.2.tar.gz"
+        sha256 "e6a112ffc14c460e8f7ff09f639499b575b4186ef0513a60d981db8add26e660"
+
+        def install
+            bin.install "exe/slidesk" => "slidesk"
+        end
+    end
+end

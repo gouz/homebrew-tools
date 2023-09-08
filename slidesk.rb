71c9ce5f0996918f2be88c4850ceaf40b39d9247
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2023-09-08 20:23:38 +0200

    fix: :ambulance: slidesk@1.4.1

--- slidesk.rb
+++ slidesk.rb
@@ -4,8 +4,8 @@ class Slidesk < Formula
     version "1.4.0"
 
     on_macos do
-        url "https://github.com/gouz/slidesk/archive/refs/tags/1.4.0.tar.gz"
-        sha256 "4e313069f62a57db80c2d0f55104257c4ab14ccc766be795bc71a14d89312929"
+        url "https://github.com/gouz/slidesk/archive/refs/tags/1.4.1.tar.gz"
+        sha256 "05d8c35d437a8fdd4a7eef82bb4e13692e192aa2209986d8878d20cef4e80289"
 
         def install
             bin.install "exe/slidesk" => "slidesk"

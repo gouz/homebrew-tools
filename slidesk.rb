0a42abbdc66fc21bbe4e1ccda8e879e10c2d5536
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2023-09-12 00:46:36 +0200

    Revert "feat: :rocket: slidesk@1.6.3"
    This reverts commit a1cc52b9c6b80eb1649c6d3bad7a5cf2693e55b6.

--- slidesk.rb
+++ slidesk.rb
@@ -1,12 +1,12 @@
 class Slidesk < Formula
     desc "Speaker companion"
     homepage "https://github.com/gouz/homebrew-tools"
-    version "1.6.3"
+    version "1.6.2"
     depends_on "bun"
 
     on_macos do
-        url "https://github.com/gouz/slidesk/releases/download/1.6.3/release_mac"
-        sha256 "974a94555bff281f4a487e41dfab7b1b322fea6fee05fddc9760a4c54e4e49d6"
+        url "https://github.com/gouz/slidesk/archive/refs/tags/1.6.2.tar.gz"
+        sha256 "2e0bcc9954161bc82ce2484225ef78fe943c05692525d058749c155b151b01fa"
 
         def install
             bin.install "exe/slidesk" => "slidesk"

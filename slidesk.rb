edb554d6790534ee8d2f39516015e81b18698425
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2023-09-12 00:35:45 +0200

    fix: :bug: sha

--- slidesk.rb
+++ slidesk.rb
@@ -6,7 +6,7 @@ class Slidesk < Formula
 
     on_macos do
         url "https://github.com/gouz/slidesk/archive/refs/tags/1.6.2.tar.gz"
-        sha256 "974a94555bff281f4a487e41dfab7b1b322fea6fee05fddc9760a4c54e4e49d6"
+        sha256 "2e0bcc9954161bc82ce2484225ef78fe943c05692525d058749c155b151b01fa"
 
         def install
             bin.install "exe/slidesk" => "slidesk"

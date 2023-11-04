1cbf05dcf914c664ce29261746ae2206d10a715a
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2023-11-05 00:53:00 +0100

    feat: :bug: rollback to 2.1.14

--- slidesk.rb
+++ slidesk.rb
@@ -1,10 +1,10 @@
 class Slidesk < Formula
     desc "Speaker companion"
     homepage "https://github.com/gouz/homebrew-tools"
-    version "2.1.15"
+    version "2.1.14"
     on_macos do
-        url "https://github.com/gouz/slidesk/releases/download/2.1.15/release_mac.tar.gz"
-        sha256 "7436c49cc721fcceb36687734f4328fcd0be3947772115cd851cba6c940c4e3d"
+        url "https://github.com/gouz/slidesk/releases/download/2.1.14/release_mac.tar.gz"
+        sha256 "c09a9baac9cc3d14355f98516f5c6809d2fbe7730e1d14a1627f3fa42d2aa778"
 
         def install
             bin.install "slidesk" => "slidesk"

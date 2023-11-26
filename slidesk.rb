3b26c2a78b915913c97998c87f837847eeaa4434
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2023-11-26 10:24:07 +0100

    feat: :rocket: slidesk@2.2.10

--- slidesk.rb
+++ slidesk.rb
@@ -1,10 +1,10 @@
 class Slidesk < Formula
     desc "Speaker companion"
     homepage "https://github.com/gouz/homebrew-tools"
-    version "2.2.9"
+    version "2.2.10"
     on_macos do
-        url "https://github.com/gouz/slidesk/releases/download/2.2.9/release_mac.tar.gz"
-        sha256 "1624b3584f76a5ed595e931ad2300aaeff2ac966dc185b90e30f7bce94f1ed42"
+        url "https://github.com/gouz/slidesk/releases/download/2.2.10/release_mac.tar.gz"
+        sha256 "181427b1d1a6de2bf9d005e57ea4156978830508210b9945e795aaedace1686f"
 
         def install
             bin.install "slidesk" => "slidesk"

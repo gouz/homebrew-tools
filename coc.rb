5dc03d89486fbe1ae8dd498b12301b74da2182d1
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2024-04-17 18:30:05 +0200

    feat: :rocket: coc@0.0.3

--- coc.rb
+++ coc.rb
@@ -1,10 +1,10 @@
 class Coc < Formula
     desc "ConferenceHall analyzer"
     homepage "https://github.com/gouz/homebrew-tools"
-    version "0.0.2"
+    version "0.0.3"
     on_macos do
-        url "https://github.com/gouz/conferencehall-orga-classment/releases/download/0.0.2/release_mac.tar.gz"
-        sha256 "f538893f47159bcf5a5598bc194d4b714eeb69e6414b890545d649558cd089b4"
+        url "https://github.com/gouz/conferencehall-orga-classment/releases/download/0.0.3/release_mac.tar.gz"
+        sha256 "89ef7310407dace8dc44c0d6aa9b6fa3dd2e363d968371e86d1a6705c3be016c"
 
         def install
             bin.install "coc" => "coc"

17c7b707eb1f7bdbf0f72cd8a4e8f7b8820e6c2e
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2024-04-20 09:49:45 +0200

    feat: :rocket: choc@0.1.9

--- choc.rb
+++ choc.rb
@@ -1,10 +1,10 @@
 class Choc < Formula
     desc "ConferenceHall organization companion"
     homepage "https://github.com/gouz/homebrew-tools"
-    version "0.1.8"
+    version "0.1.9"
     on_macos do
-        url "https://github.com/gouz/choc/releases/download/0.1.8/release_mac.tar.gz"
-        sha256 "32f448bd08a46e85bb10b2c19d250acb4b2092eb39d66a21e4fb7a6ffa16d6f3"
+        url "https://github.com/gouz/choc/releases/download/0.1.9/release_mac.tar.gz"
+        sha256 "184b3f6d7e5c800d30fe8229db0bc6204d7dc53b993a2466f1d9bb2f77109439"
 
         def install
             bin.install "choc" => "choc"

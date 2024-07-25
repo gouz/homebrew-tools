2a201b53545f2e89ed7197cd0f9cca242bb08293
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2024-07-25 20:44:47 +0200

    feat: :rocket: slidesk@2.7.1

--- slidesk.rb
+++ slidesk.rb
@@ -4,7 +4,7 @@ class Slidesk < Formula
     version "2.7.1"
     on_macos do
         url "https://github.com/gouz/slidesk/releases/download/2.7.1/release_mac.tar.gz"
-        sha256 "70307d41231e6b1cc37f0235a64a555013028109a81ddc43936503efec342cdd"
+        sha256 "0c425fbe18923754c000df1d8b72fe5c6843435d3d0fa2f5d98bddab8b794908"
 
         def install
             bin.install "slidesk" => "slidesk"

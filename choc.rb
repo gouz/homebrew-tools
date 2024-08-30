ed6dba30f67684e6e50c0e979f37af4dcd358a1a
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2024-08-30 21:40:03 +0200

    feat: :rocket: choc@0.1.13

--- choc.rb
+++ choc.rb
@@ -3,8 +3,14 @@ class Choc < Formula
     homepage "https://github.com/gouz/homebrew-tools"
     version "0.1.12"
     on_macos do
-        url "https://github.com/gouz/choc/releases/download/0.1.12/release_mac.tar.gz"
-        sha256 "cc08921169685188063abec11804f6df76d46508097e70f4105bcaedab3ddc88"
+        on_arm do
+            url "https://github.com/gouz/choc/releases/download/0.1.13/release_mac.tar.gz"
+            sha256 "c189547a510ea433acbfc0c0f98de6144ef870cdb9eb9bb92f4eb2e89d7c7dc5"
+        end
+        on_intel do
+            url "https://github.com/gouz/choc/releases/download/0.1.13/release_mac.tar.gz"
+            sha256 "bb8fb41cccac4539b770afd957bfd1bec016d609300e2b28ac616fa374e201b1"
+        end
 
         def install
             bin.install "choc" => "choc"

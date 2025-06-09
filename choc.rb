7ddcb8af3db8a9ae186811773fdd1712ccce1240
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2025-06-09 22:11:40 +0200

    Update choc.rb

--- choc.rb
+++ choc.rb
@@ -4,7 +4,7 @@ class Choc < Formula
     version "0.3.0"
     BASE_URL = "https://github.com/gouz/choc/releases/download/#{version}"
 
-    MAC_ARM_SHA = "6d2dab718916602a85a6112be91f90bb002cd97fd77bcc05947da1356f5b8627"
+    MAC_ARM_SHA = "636b326315eea3d62c762972369de449410e7cc0940bdea3f3fdecac0c60b130"
     MAC_AMD_SHA = "738e3972140b5ec3ffb536d65532130d55df9de470e480c37464bba32335d512"
     LINUX_ARM_SHA = "996965a287fec00c8fdd98f3e529cd7a30ebe9dc8294976219b97d5bcfa1a88d"
     LINUX_AMD_SHA = "02e6542cccf89f0d2a1190d3580b1ac34323d29062dee68b01b051c78d579a31"

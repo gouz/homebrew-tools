e6e6049f05ffb807581b3545cc7c16e85adf6e61
GitHub Action <>
2025-11-08 15:23:03 +0000

    Update to 2.14.8

--- slidesk.rb
+++ slidesk.rb
@@ -2,13 +2,13 @@ class Slidesk < Formula
     desc "Speaker companion"
     homepage "https://github.com/gouz/homebrew-tools"
 
-    version "2.14.7"
+    version "2.14.8"
     BASE_URL = "https://github.com/slidesk/slidesk/releases/download/#{version}"
 
-    MAC_ARM_SHA = "aaf408b0f543b4e773b01a0b01999a4ada22bc8d30ac86af069b2d0d314d56d4"
-    MAC_AMD_SHA = "3d7f3c65ac8de9e5427bbf74f2d60449764325de8d74a17f4b875d9701b75c2a"
-    LINUX_ARM_SHA = "df272b979f745311467642b54f965758796f169785fdcf3898e0a446e463c2c9"
-    LINUX_AMD_SHA = "d9302b5e85ad072684952bc19b7f62708bcae9a4f168be7264ce23320c452d4c"
+    MAC_ARM_SHA = "b20d39f7849dc639cfbc12013e13343b4c091f32ec1a0b6478e901b396c4cc0b"
+    MAC_AMD_SHA = "90f80449cb68f99291ecdea7dab41b8cc62baaef239dbcbb21b77dafdc865deb"
+    LINUX_ARM_SHA = "08bd11a731644f3c60d14c12e5975ebdf417fb6fd6121d48f6f9e90ad741c999"
+    LINUX_AMD_SHA = "eee28a1ac5b97cdfe5d9939fd38de5515ed99c891614d155b91464c18d69e426"
 
     on_macos do
         on_arm do

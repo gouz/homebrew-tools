ca74425a0f73cc2f48852de5bc722bf0e35ff58a
GitHub Action <>
2026-04-12 08:23:50 +0000

    Update to 0.0.2

--- booo.rb
+++ booo.rb
@@ -2,13 +2,13 @@ class Booo < Formula
     desc "IA companion"
     homepage "https://github.com/gouz/homebrew-tools"
 
-    version "0.0.1"
+    version "0.0.2"
     BASE_URL = "https://github.com/gouz/booo/releases/download/#{version}"
 
-    MAC_ARM_SHA = "4cd3120c4d10fd7807115de796e04dc1c91b116af619159db04f7e3539a6199e"
-    MAC_AMD_SHA = "80932ef804a68da35b24a62fc54878989e88c2efab92ecf5537df8ba66622ceb"
-    LINUX_ARM_SHA = "f6e675866d4dab408b8cd7f907c4ff347d3fceb9d04540599245a6db0724853d"
-    LINUX_AMD_SHA = "da74e9d316751b44d6c154d3019f3ad9445887d5f438338c430182aa55dd2777"
+    MAC_ARM_SHA = "64514a33df85271bb51523a98d04ea2394d47be16fc7496eae18d038cf118127"
+    MAC_AMD_SHA = "040ad7254a28527cbe4deb0b8c6652637932e6ecb22fe88ca08005dd1db12fbb"
+    LINUX_ARM_SHA = "93a73dd49c3f511fac72e680df9b7a404fef2553e45feef3c9b61df86e7d5244"
+    LINUX_AMD_SHA = "d898a204769bc57028491be0b17798d9c50faf89a29bf4cdad360f57346c8f9d"
 
     depends_on "openspec"
     depends_on "opencode"
